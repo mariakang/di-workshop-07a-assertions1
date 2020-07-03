96df5ee671a9a2077a1f37c381e12ddb5eabf7ba
mariakang <mcourtenay@gmail.com>
2020-07-03 15:24:04 +0100

    Completed exercises

--- test/exercise07.test.js
+++ test/exercise07.test.js
@@ -2,16 +2,15 @@ var chai = require('chai');
 var assert = chai.assert;
 
 
-/** Delete this line and the one at the bottom when you're ready for exercise 7
  
     suite('Unit Tests Exercise 7 - Deep Equality',function(){
       // .deepEqual(), .notDeepEqual() 
       // .deepEqual() asserts that two objects or arrays are 'deep equal'
       test('Are two shuffled objects deeply equal?', function(){
-        assert.fail( { a: '1', b: 5 } , { b: 5, a: '1' }, "keys order doesn't matter" );
+        assert.deepEqual( { a: '1', b: 5 } , { b: 5, a: '1' }, "keys order doesn't matter" );
       });
       test('Are two shuffled arrays deeply equal?', function(){
-        assert.fail( { a: [5, 6] }, { a: [6, 5] }, "array elements position does matter !!" );
+        assert.notDeepEqual( { a: [5, 6] }, { a: [6, 5] }, "array elements position does matter !!" );
       });
     })
 
@@ -20,4 +19,3 @@ var assert = chai.assert;
 // that said, arrays with the same objects in different orders
 // will not be viewed as deeply equal unless they are sorted
   
-// Delete this line when you're ready for exercise 7 **/
